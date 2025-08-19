39683a45fcb59e689ed1a03342ebe0b44b9ed26a
Ethan Berry <ethanberry670@gmail.com>
2025-08-20 00:06:02 +0100

    Added showtutorial

--- MenuManager/MenuManager.cs
+++ MenuManager/MenuManager.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using GoFish.HelperMethods;
 
 namespace GoFish.Menu
@@ -59,7 +60,7 @@ namespace GoFish.Menu
                     case MenuChoices.Tutorial:
                         Console.WriteLine("Launching tutorial...");
                         Utils.Pause(200);
-                        // ShowTutorial();
+                        ShowTutorial();
                         break;
 
                     case MenuChoices.Exit:
@@ -71,6 +72,36 @@ namespace GoFish.Menu
 
                 break;
             }
+        }
+
+        private static void ShowTutorial()
+        {
+            Console.Clear();
+            Console.WriteLine("In go fish a standard deck of 52 playing cards is used.");
+            Utils.Pause(200);
+            Console.WriteLine("Each player is dealt a hand of cards, in this version a starting hand is 7 cards.");
+            Utils.Pause(200);
+            Console.WriteLine("The remaining cards are known as the 'fish pile'.");
+            Utils.Pause(200);
+            Console.WriteLine("Players take turns asking other players for cards.");
+            Utils.Pause(200);
+            Console.WriteLine("The player asking must have at least one card of the rank they are asking for in their hand.");
+            Utils.Pause(200);
+            Console.WriteLine("For example if a player wants to ask for 'tens' they need to have at least one ten in their hand.");
+            Utils.Pause(200);
+            Console.WriteLine("If the player asked has the card, they must give all cards of that rank to the asking player. They also get another turn!");
+            Utils.Pause(200);
+            Console.WriteLine("If the player asked does not have the card they say 'Go fish!' and then the asking player draws a card from the 'fish pile'.");
+            Utils.Pause(200);
+            Console.WriteLine("If the drawn card is one they were asking for, they get another turn!");
+            Utils.Pause(200);
+            Console.WriteLine("\nWhen a player collects a set of four matching cards (a book) they score a point.");
+            Utils.Pause(200);
+            Console.WriteLine("The game ends when all cards have been collected into books or when the fish pile is empty and no one can make any more matches.");
+            Utils.Pause(200);
+            Console.WriteLine("The player with the most books(points) wins the game!");
+            Console.WriteLine("\nPress the enter key to return to the main menu: ");
+            Console.ReadLine();
         }
     }
 }
