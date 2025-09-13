584e704558604cdc5c806caaf5913c765c2154da
EthanBerry638 <ethanberry670@gmail.com>
2025-09-14 00:15:11 +0100

    Refactored url into path string for extensibility. Added logic to writetoleaderboard

--- MenuManager/LeaderBoardManager.cs
+++ MenuManager/LeaderBoardManager.cs
@@ -1,12 +1,17 @@
+using System.Text;
+
 namespace LeaderBoardManager
 {
     public class LeaderBoards
     {
-        public string[] lines = File.ReadAllLines(@"C:\Users\ethan\Desktop\Coding Projects\CSharpSelfMiniProjects\GoFishConsole\MenuManager\LeaderBoard.csv");
+        private static string path = @"C:\Users\ethan\Desktop\Coding Projects\CSharpSelfMiniProjects\GoFishConsole\MenuManager\LeaderBoard.csv";
+        public string[] lines = File.ReadAllLines(path);
 
         public void WriteToLeaderBoard(LeaderBoardEntry leaderBoardEntry)
         {
-            
+            DateTime dateTime = DateTime.Now;
+            string entry = leaderBoardEntry.Name + "/" + leaderBoardEntry.Score + "/" + dateTime;
+            File.WriteAllText(path, entry);
         }
     }
 }
