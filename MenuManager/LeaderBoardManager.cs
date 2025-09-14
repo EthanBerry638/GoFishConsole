5493adfc0a63c99c1b4fe153cb4c1314024635c7
EthanBerry638 <ethanberry670@gmail.com>
2025-09-14 01:09:10 +0100

    fixed csv formatting

--- MenuManager/LeaderBoardManager.cs
+++ MenuManager/LeaderBoardManager.cs
@@ -9,7 +9,7 @@ namespace LeaderBoardManager
         public void WriteToLeaderBoard(LeaderBoardEntry leaderBoardEntry)
         {
             DateTime dateTime = DateTime.Now;
-            string entry = leaderBoardEntry.Name + "/" + leaderBoardEntry.Score + "/" + dateTime;
+            string entry = leaderBoardEntry.Name + "," + leaderBoardEntry.Score + "," + dateTime;
             File.AppendAllText(path, entry + Environment.NewLine);
             WriteToFile();
         }
@@ -21,8 +21,8 @@ namespace LeaderBoardManager
 
             foreach (string line in lines)
             {
-                int firstIndex = line.IndexOf("/");
-                int secondIndex = line.IndexOf("/", firstIndex + 1);
+                int firstIndex = line.IndexOf(",");
+                int secondIndex = line.IndexOf(",", firstIndex + 1);
 
                 if (firstIndex != -1 && secondIndex != -1 && secondIndex > firstIndex)
                 {
@@ -45,7 +45,16 @@ namespace LeaderBoardManager
         private void WriteToFile()
         {
             List<string> sortedEntries = OrderLines();
-            File.WriteAllLines(path, sortedEntries);
+            var withHeader = new List<string> { "Rank,Name,Score,Date" };
+
+            withHeader.AddRange(sortedEntries.Select((line, index) =>
+            {
+                // Remove any existing prefix and split the original line
+                string[] parts = line.Split(',');
+                return $"{index + 1},{parts[0]},{parts[1]},{parts[2]}";
+            }));
+
+            File.WriteAllLines(path, withHeader);
         }
     }
 }
